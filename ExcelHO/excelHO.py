3797c09d97b50174fda5afb3be87aa9e9d2c8925
horropie <31204217+horropie@users.noreply.github.com>
2017-10-15 15:59:40 +0200

    Now is able to sum a category
    Somehow excel reads the added colons as semicolons?

--- ExcelHO/excelHO.py
+++ ExcelHO/excelHO.py
@@ -1,6 +1,6 @@
 import openpyxl
 from operator import is_not
-wb=openpyxl.load_workbook("Finanzen 2017_Test.xlsx")
+wb=openpyxl.load_workbook("Finanzen 2017.xlsx")
 mysheet=wb.get_sheet_by_name("Juni")
 #for col in mysheet.iter_cols(min_row=15, min_col=4, max_col=1, max_row=100):
 #    for cell in col:
@@ -18,14 +18,22 @@ for j in range(15, 100):
 
 print(listb)
 '''
-s=""
+
+
+s="=Summe("
+
 for j in range(15, 100):
     e=mysheet.cell(row=j, column=4)
     mysheet["C8"]=s
     if e.value is "l":
-        s+="l"
+        s+="C"+str(j)+","
+
+s2=s
+s2+=")"
+mysheet["C8"]=s2
+
 print(mysheet["C8"].value)
 #now still need to add the celllocation and "Summe()"
 
 
-wb.save('Finanzen 2017_Test2.xlsx')
+wb.save('Finanzen 2017_Test.xlsx')
